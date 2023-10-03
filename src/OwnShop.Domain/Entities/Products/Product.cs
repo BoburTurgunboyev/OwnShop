931c4f4c60ab485431bcf69050cdf3d8acef4bb8
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-10-04 01:28:54 +0500

    Add project files.

---
+++ src/OwnShop.Domain/Entities/Products/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.Domain.Entities.Products
+{
+    public class Product
+    {
+    }
+}
