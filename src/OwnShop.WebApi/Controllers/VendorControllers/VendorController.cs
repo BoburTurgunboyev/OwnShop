f0d2841440ce2e80e2909bef50a41d0eccfd1d1a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 21:48:08 +0500

    controller Yozyapman

---
+++ src/OwnShop.WebApi/Controllers/VendorControllers/VendorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OwnShop.WebApi.Controllers.VendorControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+    }
+}
