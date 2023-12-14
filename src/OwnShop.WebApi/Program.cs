13561bea395f5321fce6dca062d4bc38e6f6c006
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 21:47:57 +0500

    controller Yozyapman

--- src/OwnShop.WebApi/Program.cs
+++ src/OwnShop.WebApi/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using OwnShop.DataAccess.Data;
+using OwnShop.DataAccess.Interfaces.Customers;
+using OwnShop.DataAccess.Interfaces.Products;
+using OwnShop.DataAccess.Repositories.Cotegories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICustomerRepository, CostumerRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
