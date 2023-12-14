06721a5a7368d6922c6960065a9cd4e8d82bdbcb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 21:48:22 +0500

    controller Yozyapman

---
+++ src/OwnShop.WebApi/Controllers/ProductControllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OwnShop.WebApi.Controllers.ProductControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
