c753212dbcfdb5f572dec397ff948b8b01599b98
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-15 20:32:26 +0500

    configration yozildi

--- src/OwnShop.Domain/Entities/Customers/Customer.cs
+++ src/OwnShop.Domain/Entities/Customers/Customer.cs
@@ -1,10 +1,6 @@
 ﻿using OwnShop.Domain.Entities.Products;
 using OwnShop.Domain.Entities.Sales;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace OwnShop.Domain.Entities.Customers
 {
