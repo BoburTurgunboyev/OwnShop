d4200a8bc829375ab50e6bb2a58a35fc874409cf
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-15 20:33:14 +0500

    configration yozildi

---
+++ src/OwnShop.DataAccess/Configrations/ProductConfigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OwnShop.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.DataAccess.Configrations
+{
+    public class ProductConfigration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> entity)
+        {
+
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.Name).IsRequired().HasMaxLength(255);
+            entity.Property(p => p.Price).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(p => p.Customer)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CostumerId )
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            
+        }
+    }
+}
