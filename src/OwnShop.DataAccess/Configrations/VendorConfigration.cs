906e09a888bf2927c9f0fec551ba0cfeae63e8c5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-15 20:33:02 +0500

    configration yozildi

---
+++ src/OwnShop.DataAccess/Configrations/VendorConfigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OwnShop.Domain.Entities.Vendors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.DataAccess.Configrations
+{
+    public class VendorConfigration : IEntityTypeConfiguration<Vendor>
+    {
+        public void Configure(EntityTypeBuilder<Vendor> entity)
+        {
+            entity.HasKey(v => v.Id);
+            entity.Property(v => v.Name).IsRequired().HasMaxLength(255);
+
+            entity.HasMany(v => v.Sales)
+                .WithOne(s => s.Vendor)
+                .HasForeignKey(s => s.VendorId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
