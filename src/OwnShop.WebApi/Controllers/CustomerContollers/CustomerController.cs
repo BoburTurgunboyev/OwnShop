0c6a1738d277e7b4faeabcba92f8ec72f5fd8283
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-15 21:58:49 +0500

    crud boshlandi

--- src/OwnShop.WebApi/Controllers/CustomerContollers/CustomerController.cs
+++ src/OwnShop.WebApi/Controllers/CustomerContollers/CustomerController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OwnShop.Service.Dtos.Customers;
+using OwnShop.Service.Interfaces.Customers;
 
 namespace OwnShop.WebApi.Controllers.CustomerContollers
 {
@@ -7,5 +9,27 @@ namespace OwnShop.WebApi.Controllers.CustomerContollers
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        private readonly ICustomersService _customersService;
+
+        public CustomerController(ICustomersService customersService)
+        {
+            _customersService = customersService;
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetAll()
+        {
+            var res = await _customersService.GetAlldAsync();
+            return Ok(res);
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateCustomer(CustomerDto customerDto)
+        {
+            var res = await _customersService.CreateAsync(customerDto);
+            return Ok(res);
+        }
     }
 }
