2634d4e44fb2e38a6ead4f9d58e8b6c7de406c25
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-15 20:33:18 +0500

    configration yozildi

---
+++ src/OwnShop.DataAccess/Configrations/CustomerConfigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OwnShop.Domain.Entities.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.DataAccess.Configrations
+{
+    public class CustomerConfigration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> entity)
+        {
+            entity.ToTable("Customer");
+
+            entity.HasKey(c => c.Id);
+
+            entity.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(c => c.PhoneNum)
+                .HasMaxLength(20);
+
+            entity.Property(c => c.Address)
+                .HasMaxLength(200);
+
+            entity.HasMany(c => c.Products)
+                .WithOne(p => p.Customer)
+                .HasForeignKey(p => p.CostumerId);
+
+            entity.HasMany(c => c.Sales)
+                .WithOne(s => s.Customer)
+                .HasForeignKey(s => s.CustomerId);
+        }
+    }
+}
