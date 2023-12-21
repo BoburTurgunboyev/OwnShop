56ed99716e026b70450b78c811d8cec4dc5cc058
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 17:48:52 +0500

    crud

--- src/OwnShop.WebApi/Controllers/ShopControllers/ShopController.cs
+++ src/OwnShop.WebApi/Controllers/ShopControllers/ShopController.cs
@@ -1,11 +1,64 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OwnShop.Service.Dtos.Shops;
+using OwnShop.Service.Interfaces.Shops;
 
 namespace OwnShop.WebApi.Controllers.ShopControllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ShopController : ControllerBase
     {
+        private readonly IShopService _shopsService;
+
+        public ShopController(IShopService shopsService)
+        {
+            _shopsService = shopsService;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllShop()
+        {
+            var res = await _shopsService.GetAllAsync();
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateShop(ShopDto shop)
+        {
+            var res = await _shopsService.CreateAsync(shop);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdShop(int id)
+        {
+            var res = await _shopsService.GetByIdAsync(id);
+            return Ok(res);
+        }
+        [HttpPut]
+
+        public async ValueTask<IActionResult> UpdateShop(long id,ShopDto shop)
+        {
+            var res = await _shopsService.UpdateAsync(id, shop);
+            return Ok(res);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteShop(long id)
+        {
+            var res = await _shopsService.DeleteAsync(id);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> CountShop() 
+        {
+            var res = await _shopsService.CountAsync();
+            return Ok(res);
+        }
+
+
+        
     }
 }
