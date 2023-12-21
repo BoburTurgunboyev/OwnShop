3805e1cc13f3fd1d16e682fff1a5279833cf4d22
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 18:16:38 +0500

    migration

---
+++ src/OwnShop.DataAccess/Migrations/20231221131020_Initial1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OwnShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
