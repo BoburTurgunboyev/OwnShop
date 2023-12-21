843262bdafc7fd4412e00756a7cd04fdb44007c3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 21:37:43 +0500

    Role berdim

---
+++ src/OwnShop.DataAccess/Migrations/20231221161430_Initial11.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OwnShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Customer",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Customer");
+        }
+    }
+}
