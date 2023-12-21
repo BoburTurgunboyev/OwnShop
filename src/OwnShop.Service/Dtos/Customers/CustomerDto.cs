bc7f56f600e2a5dfb6f55eb34726c3b64eb4c47f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 21:37:27 +0500

    Role berdim

--- src/OwnShop.Service/Dtos/Customers/CustomerDto.cs
+++ src/OwnShop.Service/Dtos/Customers/CustomerDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OwnShop.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,8 @@ namespace OwnShop.Service.Dtos.Customers
         public string Name { get; set; } = string.Empty;
         public string PhoneNum { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
     }
 }
