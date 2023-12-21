f9d6f307f263aa75c74c2e4308cec56e8b2e4f94
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 21:37:10 +0500

    Role berdim

--- src/OwnShop.WebApi/Program.cs
+++ src/OwnShop.WebApi/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using OwnShop.DataAccess.Data;
 using OwnShop.DataAccess.Interfaces.Customers;
@@ -12,11 +14,13 @@ using OwnShop.Service.Interfaces.Products;
 using OwnShop.Service.Interfaces.Sales;
 using OwnShop.Service.Interfaces.Shops;
 using OwnShop.Service.Interfaces.Vendors;
+using OwnShop.Service.JWT.AuthServices;
 using OwnShop.Service.Service.Customers;
 using OwnShop.Service.Service.Products;
 using OwnShop.Service.Service.Sales;
 using OwnShop.Service.Service.Shops;
 using OwnShop.Service.Service.Vendors;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +41,8 @@ builder.Services.AddScoped<ISaleService,SaleService>();
 builder.Services.AddScoped<IShopService,ShopService>();
 builder.Services.AddScoped<IVendorService,VendorService>();
 
+builder.Services.AddScoped<IAuthService,  AuthService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -44,40 +50,75 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options
     => options.UseSqlServer(builder.Configuration.GetConnectionString("Connected")));
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProject", Version = "v1.0.0" });
-    var securitySchema = new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-    c.AddSecurityDefinition("Bearer", securitySchema);
-    var securityRequirement = new OpenApiSecurityRequirement
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opttins =>
                 {
-                    { securitySchema, new[] { "Bearer" } }
-                };
-    c.AddSecurityRequirement(securityRequirement);
+                    opttins.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        // kimga chiqarilgan
+                        ValidateIssuer = true,
+                        // kim tomonidan berilgan
+                        ValidateAudience = true,
+                        // vaqti
+                        ValidateLifetime = true,
+                        // secret keyi
+                        ValidateIssuerSigningKey = true,
+                        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+                    };
+                });
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "AuthDemo",
+        Description = "Auth Demo Description"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        Description = "Bearer Authentication",
+        Type = SecuritySchemeType.Http
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string> ()
+                    }
+
+                });
 });
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/V1/swagger.json", "Auth Demo API");
+    });
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthentication();
 
 app.UseAuthorization();
