9f27b3c59dd89666a00ce9fc87ea9bf7a88e9616
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 20:59:47 +0500

    jwt qoshildi

---
+++ JwtTokken/JWT/AuthServices/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using OwnShop.Service.Dtos.Login;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace OwnShop.Service.JWT.AuthServices
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(LoginDto loginDto)
+        {
+            var claims = new Claim[]
+          {
+                // name 
+                new Claim("PhoneNumber", loginDto.PhoneNumber),
+                // identificatori
+                new Claim("Password",loginDto.Password),
+                new Claim(ClaimTypes.Role,loginDto.Role.ToString()),
+                // vaqti
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToString()),
+
+          };
+
+            // qandedur algoritm boyicha shifrlanadi
+            var credentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"])),
+                SecurityAlgorithms.HmacSha256
+                );
+
+            var token = new JwtSecurityToken(
+                _configuration["JWT:ValidIssuer"],
+                _configuration["JWT:ValidAudience"],
+                claims,
+                expires: DateTime.Now.AddSeconds(60),
+                signingCredentials: credentials
+                );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            return tokenHandler.WriteToken(token);
+
+
+        }
+    }
+}
