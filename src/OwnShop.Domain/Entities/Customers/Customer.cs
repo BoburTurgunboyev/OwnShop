e151ddbaecd235d19b052b00b913c060d33a7610
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 20:59:34 +0500

    jwt qoshildi

--- src/OwnShop.Domain/Entities/Customers/Customer.cs
+++ src/OwnShop.Domain/Entities/Customers/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using OwnShop.Domain.Entities.Products;
 using OwnShop.Domain.Entities.Sales;
+using OwnShop.Domain.Enums;
 
 
 namespace OwnShop.Domain.Entities.Customers
@@ -11,6 +12,8 @@ namespace OwnShop.Domain.Entities.Customers
         public string Name { get; set; } = string.Empty;
         public string PhoneNum { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public Role Role { get; set; }
 
         public ICollection<Product> Products { get; set; }
         public ICollection<Sale> Sales { get; set; }
