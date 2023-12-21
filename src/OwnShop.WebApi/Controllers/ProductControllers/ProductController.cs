b939a5bf6ae334b1e1ebfc05e5d4041f4a2ca1d4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 17:49:04 +0500

    crud

--- src/OwnShop.WebApi/Controllers/ProductControllers/ProductController.cs
+++ src/OwnShop.WebApi/Controllers/ProductControllers/ProductController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OwnShop.DataAccess.Interfaces.Products;
+using OwnShop.DataAccess.Repositories.Cotegories;
+using OwnShop.Domain.Entities.Products;
+using OwnShop.Service.Dtos.Products;
+using OwnShop.Service.Interfaces.Products;
 
 namespace OwnShop.WebApi.Controllers.ProductControllers
 {
@@ -7,5 +12,56 @@ namespace OwnShop.WebApi.Controllers.ProductControllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllProduct() 
+        {
+            var res = await _productService.GetAllAsync();
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateProduct(ProductDto productDto)
+        {
+            var res = await _productService.CreateAsync(productDto);
+            return Ok(res);
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetByIdProduct(int id)
+        {
+            var res = await _productService.GetByIdAsync(id);
+            return Ok(res);
+        }
+
+        [HttpPut]
+
+        public async ValueTask<IActionResult> UpdateProduct(long id ,ProductDto productDto)
+        {
+            var res = await _productService.UpdateAsync(id,productDto);
+            return Ok(res);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteProduct(long id)
+        {
+            var res = await _productService.DeletePASync(id);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> CountProduct() 
+        {
+            var res = await _productService.CountAsync();
+            return Ok(res);
+        }
+
     }
 }
