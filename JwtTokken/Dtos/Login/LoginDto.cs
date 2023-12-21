8a9f99c6015b23cf119a2ccb80f40371d8c40566
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 20:59:55 +0500

    jwt qoshildi

---
+++ JwtTokken/Dtos/Login/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿
+using OwnShop.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.Service.Dtos.Login
+{
+    public class LoginDto
+    {
+       
+
+        public string PhoneNumber { get; set; }   
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
