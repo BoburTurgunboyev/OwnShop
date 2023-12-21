d02e8f3553e74192c291c6ab5339730937ea5b9d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 20:59:51 +0500

    jwt qoshildi

---
+++ JwtTokken/Hash/Hash512.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.Service.Hash
+{
+    public class Hash512
+    {
+        public static string ComputeHash512(string password)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+
+                byte[] hashBytes = sha512.ComputeHash(inputBytes);
+
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+    }
+}
