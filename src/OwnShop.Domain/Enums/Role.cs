e86acebdfb91932c8d1ea66e6d66af8ac72d1ca3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-21 20:59:29 +0500

    jwt qoshildi

---
+++ src/OwnShop.Domain/Enums/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnShop.Domain.Enums
+{
+    public enum Role
+    {
+        Admin,
+        SuperAdmin,
+        Customer,
+        Vendor,
+        
+    }
+}
